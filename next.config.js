d0f3eea34ebc9714a5d8b2aa94d034957b00d704
Tharit Mohamed Hussain Khan <Zlac177@live.rhul.ac.uk>
2025-09-14 23:17:56 +0700

    fix: Clean up next.config.js to remove invalid configuration options

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false
-  },
-  api: {
-    responseLimit: false,
-  },
   env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
+    KPIT_API_KEY: process.env.KPIT_API_KEY,
+    KPIT_API_BASE_URL: process.env.KPIT_API_BASE_URL,
+    KPIT_MODEL_NAME: process.env.KPIT_MODEL_NAME,
   }
 }
 
